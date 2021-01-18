a6e7ba47a8a72a3c0311e582ebcea2941acb96a0
A597459 <76664505+A597459@users.noreply.github.com>
2021-01-18 23:15:01 +0530

    Array in Python

---
+++ Array.py
@@ -0,0 +1,15 @@
+# Create an array:
+flower = ["rose", "lilly", "sunflower"]
+print(flower)
+
+# Get the value of the first array item:
+flower = ["rose", "lilly", "sunflower"]
+x = flower[0]
+print(x)
+
+# Modify the value of the first array item:
+flower = ["rose", "lilly", "sunflower"]
+flower[0] = "daffodil"
+print(flower)
+
+# 
