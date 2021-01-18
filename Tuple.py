0cab6b62a72fd34d78ac439e34805b2b728e0aa7
A597459 <76664505+A597459@users.noreply.github.com>
2021-01-18 19:06:55 +0530

    Tuple in Python

---
+++ Tuple.py
@@ -0,0 +1,37 @@
+# Create a Tuple
+from typing import Any
+
+thistuple = ("Apple", "banana", "orange", "mango")
+print(thistuple)
+
+# Print the number of item
+thistuple = ("Apple", "banana", "mango", "orange")
+print(len(thistuple))
+
+# One item tuple, remember the commma (Dont miss COMMA)
+thistuple = ("Apple",)
+print(type(thistuple))
+
+# NOT a tuple
+thistuple = "Apple"
+print(type(thistuple))
+
+# print string,int and boolean Data type
+tuple = ("Apple", "banana", "mango", "cherry")
+tuple1 = (1, 3, 4, 2, 7)
+tuple2 = (True, False, True)
+print(tuple)
+print(tuple1)
+print(tuple2)
+
+# A tuple with str,int and booleans value
+tuple1 = (50, "Apple", True, "mango", False)
+print(tuple1)
+
+# what is the data type of tuple
+mytuple = ("Apple", "bnana", "mango", "orange")
+print(type(mytuple))
+
+# using the tuple method() to make a tuple.  note: Don't Forget Double cote(())
+thistuple = ("apple", "mango", "orange")
+print(thistuple)
